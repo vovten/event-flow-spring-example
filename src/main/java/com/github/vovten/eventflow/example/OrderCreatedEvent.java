5dbde85aaec008151022507d9480fe134bfa8b15
Vladimir Aleshkov <vovten@mail.ru>
2026-03-14 14:58:35 +0600

    Update event-flow configuration and event handling

--- src/main/java/com/github/vovten/eventflow/example/OrderCreatedEvent.java
+++ src/main/java/com/github/vovten/eventflow/example/OrderCreatedEvent.java
@@ -1,4 +1,21 @@
 package com.github.vovten.eventflow.example;
 
-public class OrderCreatedEvent {
+import com.github.vovten.eventflow.event.AbstractTraceableEvent;
+import com.github.vovten.eventflow.event.Event;
+
+public class OrderCreatedEvent extends AbstractTraceableEvent {
+    private final Long orderId;
+
+    public OrderCreatedEvent(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    @Override
+    public Class<? extends Event> type() {
+        return OrderCreatedEvent.class;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
 }
--- src/main/java/com/github/vovten/eventflow/example/OrderEventListener.java
+++ src/main/java/com/github/vovten/eventflow/example/OrderEventListener.java
@@ -1,4 +1,13 @@
 package com.github.vovten.eventflow.example;
 
+import com.github.vovten.eventflow.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderEventListener {
+
+    @EventListener
+    public void onEvent(OrderCreatedEvent event) {
+        System.out.println("Order created: " + event.getOrderId());
+    }
 }
